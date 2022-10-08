7dacc011f6b730824cd5973c5818817975e22faa
commander12368900 <115082674+commander12368900@users.noreply.github.com>
2022-10-08 23:07:21 +0500

    lol
    mf

--- settings.js
+++ settings.js
@@ -19,26 +19,26 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['923084713397']
+global.premium = ['2348163001531']
+global.ownernomer = '𝘾𝞗𝞛𝞛𝞓𝞜𝘿𝞢𝞒'
+global.ownername = '𓅰 𝘾𝞗𝞛𝞛𝞓𝞜𝘿𝞢𝞒'
+global.botname = 'bot-𝘾𝞗𝞛𝞛𝞓𝞜𝘿𝞢𝞒'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'one piece, Pakistan'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.packname = '𝘾𝞗𝞛𝞛𝞓𝞜𝘿𝞢𝞒-bot'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
+    admin: 'are u admin? dumb mf!',
+    botAdmin: 'did u make me admin? mf?!',
+    owner: 'This Feature Is Only For commander!',
     group: 'Feature Used Only For Groups!',
     private: 'Features Used Only For Private Chat!',
     bot: 'This Feature Is Only For Bot',
