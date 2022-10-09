7886a640a129991cc8683555222f46efdf066e6d
commander12368900 <115082674+commander12368900@users.noreply.github.com>
2022-10-09 20:19:41 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,14 +19,14 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['2348089700175]
+global.premium = ['2348089700175']
+global.ownernomer = '2348089700175'
+global.ownername = 'obbizzy'
+global.botname = 'OBBIZZY BOT'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'Nigeria,Kano'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
